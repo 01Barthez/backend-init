f60d48934cf5b936ad8b5bd1ebc8f3eb3ae00756
01Barthez <kenwoubarthez@gmail.com>
2025-09-02 09:08:32 +0100

    fix: update packge jsob fike

--- commitlint.config.js
+++ commitlint.config.js
@@ -18,6 +18,18 @@ module.exports = {
         'revert',
       ],
     ],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'scope-case': [0, 'always', 'lower-case'],
+    'scope-empty': [1, 'never'], // ⚠️ warning instead of error
+    'subject-case': [0, 'never'],
+    'subject-max-length': [2, 'always', 100],
+  },
+  prompt: {
+    messages: {
+      type: 'Select the type of commit:',
+      scope: 'Define the scope (optional):',
+      subject: 'Write a short and clear subject:',
+      body: 'Detailed description (optional):',
+      footer: 'References or tickets (optional):',
+    },
   },
 };
